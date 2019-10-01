d5fab5bbbc5261733d1c24f18d8758be70dd36af
Budin Kolyandov <49945848+BudinKolyandov@users.noreply.github.com>
2019-10-01 13:22:16 +0300

    Completed problems 9-11

--- DataAggregationExercise.sql
+++ DataAggregationExercise.sql
@@ -65,3 +65,45 @@ SELECT DepositGroup, MagicWandCreator, MIN(DepositCharge) AS MinDepositCharge
 FROM WizzardDeposits
 GROUP BY DepositGroup, MagicWandCreator
 ORDER BY MagicWandCreator, DepositGroup;
+
+/*
+    Problem 9. Age Groups
+*/
+
+SELECT *, COUNT(*) AS WizardCount FROM
+(
+SELECT
+	CASE 
+		WHEN Age <= 10 THEN '[0-10]'
+		WHEN Age BETWEEN 11 AND 20 THEN '[11-20]'
+		WHEN Age BETWEEN 21 AND 30 THEN '[21-30]'
+		WHEN Age BETWEEN 31 AND 40 THEN '[31-40]'
+		WHEN Age BETWEEN 41 AND 50 THEN '[41-50]'
+		WHEN Age BETWEEN 51 AND 60 THEN '[51-60]'
+		ELSE '[61+]'
+	END AS AgeGroup 
+	FROM WizzardDeposits
+)AS t
+GROUP BY AgeGroup
+ORDER BY AgeGroup
+
+/*
+    Problem 10. First Letter
+*/
+
+SELECT LEFT (FirstName, 1) AS FirstLetter 
+FROM WizzardDeposits
+WHERE DepositGroup = 'Troll Chest'
+GROUP BY LEFT (FirstName, 1)
+
+/*
+    Problem 11. Average Interest
+*/
+
+SELECT DepositGroup, 
+IsDepositExpired, 
+AVG (DepositInterest)
+FROM WizzardDeposits
+WHERE DATEPART(YEAR, DepositStartDate) >= 1985
+GROUP BY DepositGroup, IsDepositExpired
+ORDER BY DepositGroup DESC, IsDepositExpired
